79af4870fb6a1813f84861e8431d177409902244
SamsulAripin77 <arifins192@gmail.com>
2024-11-04 19:50:04 +0700

    registration success ok constant vus

--- registrasi/constant.js
+++ registrasi/constant.js
@@ -1,56 +1,43 @@
 import { check, sleep } from 'k6';
 import { registerUser } from '../helpers/user.js';
 import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
+import { Counter } from 'k6/metrics';
 
 
-export let options = {
+export const options = {
     scenarios: {
         userRegistration: {
             exec: 'userRegistration',
             executor: 'constant-vus',
-            vus: 50,               
+            vus: 60,               
             duration: '1m',  
         },
     },
     thresholds: {
-        'http_req_failed': ['rate<0.10'], 
-        'http_req_duration': ['p(90)<6000'], 
-        'checks': ['rate>0.90'], 
+        user_registration_counter_success: ['count>90'],
+        user_registration_counter_error: ['count<10'],
     },
 };
 
-let executed = {};  
+const registerCounterSuccess = new Counter("user_registration_counter_success");
+const registerCounterError = new Counter("user_registration_counter_error");
 
 export function userRegistration() {
-    const vuId = __VU; 
-
-    if (executed[vuId]) {
-        sleep(1); 
-        return;
-    }
-
-    executed[vuId] = true;  
     const uniqueId = uuidv4();
     const registerRequest = {
         fullName: "string",
-        email: `kasir_uuid_${uniqueId}@gmail.com`,
+        email: `_kasir_uuid_${uniqueId}@gmail.com`,
         password: 'noekasep@123OK!!',
         retryPassword: 'noekasep@123OK!!',
         role: "Owner",
         storeName: "string"
     };
-    const res = registerUser(registerRequest);
-
-    console.log('Response Body:', res.body);
-
-    check(res, {
-        'is status 200': (r) => r.status === 200,
-        'registration successful': (r) => {
-            const jsonResponse = JSON.parse(r.body);
-            console.log('Registration Response:', jsonResponse);
-            return jsonResponse.message === 'Pendaftaran Berhasil';
-        },
-    });
+    const registerResponse = registerUser(registerRequest);
+    if (registerResponse.status === 200) {
+        registerCounterSuccess.add(1);
+    } else {
+        registerCounterError.add(1);
+    }
 
     sleep(1);  
 }
